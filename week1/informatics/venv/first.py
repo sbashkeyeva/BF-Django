fbe2206b52af1eee0497ab891453fe8b708492f8
Symbat <sbashkeyeva@gmail.com>
2018-09-03 11:59:22 +0600

    city of stars

---
+++ week1/informatics/venv/first.py
@@ -0,0 +1,5 @@
+import math
+a=int(input())
+b=int(input())
+hep=float(math.sqrt(float(a*a)+float(b*b)))
+print(hep)
---
+++ week1/informatics/venv/second.py
@@ -0,0 +1,5 @@
+x = int(input())
+print('The next number for the number ' + str(x) + ' is ' + str(x + 1) + '.')
+print('The previous number for the number ' + str(x) + ' is ' + str(x - 1) + '.')
+
+
